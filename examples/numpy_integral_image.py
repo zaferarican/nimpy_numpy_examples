7ca13352267731f738acaf618268ae605d19cdb0
Zafer ARICAN <zafer@orangepi>
2019-03-03 23:08:03 +0300

    For integral image example, elapsed times for both approaches are now averaged over 100 trials
    trailing whitespaces are removed in nimpy_integral.nim

--- examples/numpy_integral_image.py
+++ examples/numpy_integral_image.py
@@ -2,29 +2,45 @@ import cv2
 import numpy as np
 import time
 # Generated module using nimpy
-import nimpy_integral as ni 
+import nimpy_integral as ni
 
 print("""
 
 Integral image example and comparison to OpenCV integral image function
 
 """)
-a = np.random.randint(0,high=255, size=(400,100), dtype=np.uint8)
+duration_cv2 = 0
+duration_nim = 0
+num_of_trials = 100
+for _ in range(num_of_trials):
+    a = np.random.randint(0,high=255, size=(400,100), dtype=np.uint8)
+
+    start_time = time.time()
+    sum_val = np.zeros(a.shape, np.int32)
+    a_int = cv2.integral(a, sum_val, -1)
+    a_int = a_int[1:,1:]
+    duration_cv2 += time.time() - start_time
+
+    start_time = time.time()
+    b = a[:].astype(np.int32)
+    ni.integral(b)
+    duration_nim += time.time()-start_time
+
+print("Num Of Trials: " + str(num_of_trials))
+print("OpenCV - Average time: " + str(duration_cv2 / num_of_trials))
+print("Nim    - Average time: " + str(duration_nim / num_of_trials))
+print(" ")
 
 print("**********************************")
 print("OpenCV integral image")
-start_time = time.time()
 sum_val = np.zeros(a.shape, np.int32)
 a_int = cv2.integral(a, sum_val, -1)
 a_int = a_int[1:,1:]
-print(time.time()-start_time)
 print(a_int[0:5,0:5])
 print(" ")
 print("**********************************")
 print("Custom integral image using nimpy")
-start_time = time.time()
 b = a[:].astype(np.int32)
 ni.integral(b)
-print(time.time()-start_time)
 print(b[0:5,0:5])
 
--- nimpy_numpy_examples/nimpy_integral.nim
+++ nimpy_numpy_examples/nimpy_integral.nim
@@ -1,12 +1,12 @@
 import nimpy
 import nimpy/raw_buffers
 
-proc `+`[T](p: ptr T, val: int) : ptr T {.inline.} =                                                                                                 
-  cast[ptr T](cast[uint](p) + cast[uint](val * sizeof(T)))   
+proc `+`[T](p: ptr T, val: int) : ptr T {.inline.} =
+  cast[ptr T](cast[uint](p) + cast[uint](val * sizeof(T)))
 
 # Operator [] overloading for type int32 (cint) to use for numpy buffer
-proc `[]`(p:RawPyBuffer, y:uint32, x:uint32):cint {.inline.} = 
-  cast[ptr UncheckedArray[cint]](p.buf)[y * (p.shape + 1)[].uint32 + x]  
+proc `[]`(p:RawPyBuffer, y:uint32, x:uint32):cint {.inline.} =
+  cast[ptr UncheckedArray[cint]](p.buf)[y * (p.shape + 1)[].uint32 + x]
 
 proc `[]=`(p: RawPyBuffer, y:uint32, x:uint32, val:cint) {.inline.} = 
   cast[ptr UncheckedArray[cint]](p.buf)[y * (p.shape + 1)[].uint32 + x] = val
@@ -14,7 +14,7 @@ proc `[]=`(p: RawPyBuffer, y:uint32, x:uint32, val:cint) {.inline.} =
 
 #[
   Computation of in-place integral image using Viola Recursion Method:
-  Viola, P.; Jones, M. Rapid Object Detection using a Boosted Cascade of Simple Features. 
+    Viola, P.; Jones, M. Rapid Object Detection using a Boosted Cascade of Simple Features. 
   In Proceedings of the IEEE Computer Society Conference on Computer Vision and Pattern Recognition, 
   Kauai, HI, USA, 8–14 December 2001; pp. 511–518.
 
@@ -25,7 +25,7 @@ proc integral(o: PyObject) {.exportpy.} =
 
   var aBuf: RawPyBuffer
   o.getBuffer(aBuf, PyBUF_WRITABLE or PyBUF_ND)
-  
+
   let rows = aBuf.shape[].uint32
   let columns = (aBuf.shape + 1)[].uint32
 
@@ -33,17 +33,17 @@ proc integral(o: PyObject) {.exportpy.} =
   var s_c = s_o
 
   for row in 1..<rows:
-     s_o = aBuf[row, 0]
-     aBuf[row, 0] = aBuf[(row-1), 0] + s_o
+    s_o = aBuf[row, 0]
+    aBuf[row, 0] = aBuf[(row-1), 0] + s_o
 
   for column in 1..<columns:
-     s_o = aBuf[0, column] + s_c
-     s_c = s_o
-     aBuf[0, column] = s_o
+    s_o = aBuf[0, column] + s_c
+    s_c = s_o
+    aBuf[0, column] = s_o
 
   for row in 1..<rows:
-     s_c = aBuf[row, 0] - aBuf[(row-1), 0]
-     for column in 1..<columns:
+    s_c = aBuf[row, 0] - aBuf[(row-1), 0]
+    for column in 1..<columns:
        s_o = aBuf[row, column] + s_c
        aBuf[row, column] = aBuf[(row-1), column] + s_o
        s_c = s_o

585a793bac61172d1c3a8f1b1fcdd170cecdb113
Zafer Arıcan <zafer.arican@gmail.com>
2019-03-07 14:35:35 +0300

    copyMem parts are now moved to toTensor and toPyObject

--- nimpy_numpy_examples/nimpy_integral_arraymancer.nim
+++ nimpy_numpy_examples/nimpy_integral_arraymancer.nim
@@ -6,32 +6,24 @@ import arraymancer
 proc `+`[T](p: ptr T, val: int) : ptr T {.inline.}=
   cast[ptr T](cast[uint](p) + cast[uint](val * sizeof(T)))
 
-# Operator [] overloading for type int32 (cint) to use for numpy buffer
-
-proc `[]`(p:RawPyBuffer, x:uint32):cint {.inline.} =
-  cast[ptr UncheckedArray[cint]](p.buf)[x]
-
-proc `[]=`(p: RawPyBuffer, x:uint32, val:cint) {.inline.} =
-  cast[ptr UncheckedArray[cint]](p.buf)[x] = val
-
-
 proc toTensor*(aBuf: RawPyBuffer):Tensor[cint] =
-  let rows = aBuf.shape[].int32
-  let columns = (aBuf.shape + 1)[].int32
-  var seq_xy = newSeq[cint](rows * columns)
-  for i in 0..<(rows * columns):
-    seq_xy[i] = aBuf[uint32(i)]
-  result = seq_xy.toTensor.reshape(int(rows), int(columns)) 
+  let rows = aBuf.shape[].int
+  let columns = (aBuf.shape + 1)[].int
+  result = newTensor[cint](rows,columns)
+  var bufPtr = cast[ptr UncheckedArray[cint]](aBuf.buf)
+  var tensorDataPtr = cast[ptr UncheckedArray[cint]](result.get_data_ptr)
+  copyMem(tensorDataPtr, bufPtr, rows*columns*sizeof(cint))
 
-proc toPyObject*(t: Tensor[cint], aBuf: RawPyBuffer) = 
-  let rows = aBuf.shape[].uint32
-  let columns = (aBuf.shape + 1)[].uint32
-  for coord,v in t:
-      aBuf[uint32(coord[0]) * columns + uint32(coord[1])] = v
+proc toPyObject*(t: Tensor[cint], aBuf: RawPyBuffer) =
+  let rows = aBuf.shape[].int
+  let columns = (aBuf.shape + 1)[].int
+  var bufPtr = cast[ptr UncheckedArray[cint]](aBuf.buf)
+  var tensorDataPtr = cast[ptr UncheckedArray[cint]](t.get_data_ptr)
+  copyMem(bufPtr, tensorDataPtr, rows*columns*sizeof(cint))
 #[
   Computation of in-place integral image using Viola Recursion Method:
-    Viola, P.; Jones, M. Rapid Object Detection using a Boosted Cascade of Simple Features. 
-  In Proceedings of the IEEE Computer Society Conference on Computer Vision and Pattern Recognition, 
+    Viola, P.; Jones, M. Rapid Object Detection using a Boosted Cascade of Simple Features.
+  In Proceedings of the IEEE Computer Society Conference on Computer Vision and Pattern Recognition,
   Kauai, HI, USA, 8–14 December 2001; pp. 511–518.
 
   It assumes that the numpy array has dtype in np.int32
@@ -43,10 +35,7 @@ proc integral(o: PyObject) {.exportpy.} =
   let rows = aBuf.shape[].int
   let columns = (aBuf.shape + 1)[].int
 
-  var integral_tensor = newTensor[cint](rows,columns)
-  var bufPtr = cast[ptr UncheckedArray[cint]](aBuf.buf)
-  var tensorDataPtr = cast[ptr UncheckedArray[cint]](integral_tensor.get_data_ptr)
-  copyMem(tensorDataPtr, bufPtr, rows*columns*sizeof(cint))
+  var integral_tensor = aBuf.toTensor
 
   var s_o = integral_tensor[0,0]
   var s_c = s_o
@@ -66,5 +55,5 @@ proc integral(o: PyObject) {.exportpy.} =
       s_o = integral_tensor[row, column] + s_c
       integral_tensor[row, column] = integral_tensor[(row-1), column] + s_o
       s_c = s_o
-  copyMem(bufPtr, tensorDataPtr, rows * columns * sizeof(cint))
+  integral_tensor.toPyObject(aBuf)
   aBuf.release()
